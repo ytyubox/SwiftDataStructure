bcff52c7a766c33324c980bb2fb437723c527f60
Tsungyu Yu <ytyubox@gmail.com>
2020-02-05 13:39:22 +0800

    add node doc

--- Sources/SwiftDataStructure/SwiftDataStructure.swift
+++ Sources/SwiftDataStructure/SwiftDataStructure.swift
@@ -1,9 +1,13 @@
+/// Referance type of all a single node
+///
 public final class Node<Element> {
 	public init(value: Element, next: Node<Element>? = nil) {
 		self.value = value
 		self.next = next
 	}
 	
+	/// the value of this node
 	public var value:Element
+	/// the referance of next node
 	public var next:Node?
 }
