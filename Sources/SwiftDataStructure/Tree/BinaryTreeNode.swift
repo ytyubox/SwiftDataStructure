68b04b2eb6f3fa5b0ed0268bf885b700b56523af
Tsungyu Yu <ytyubox@gmail.com>
2020-02-09 16:02:08 +0800

    add binaryTreeNode

---
+++ Sources/SwiftDataStructure/Tree/BinaryTreeNode.swift
@@ -0,0 +1,80 @@
+/**
+A general-purpose binary tree.
+Nodes don't have a reference to their parent.
+*/
+public indirect enum BinaryTreeNode<T> {
+	case node(BinaryTreeNode<T>, T, BinaryTreeNode<T>)
+	case empty
+	
+	public var count: Int {
+		switch self {
+		case let .node(left, _, right):
+			return left.count + 1 + right.count
+		case .empty:
+			return 0
+		}
+	}
+}
+
+extension BinaryTreeNode: CustomStringConvertible {
+	public var description: String {
+		switch self {
+		case let .node(left, value, right):
+			return "value: \(value), left = [\(left.description)], right = [\(right.description)]"
+		case .empty:
+			return ""
+		}
+	}
+}
+
+extension BinaryTreeNode {
+	public enum TraverseOption {
+		case pre,`in`, post
+	}
+	public func traverse(by option:TraverseOption,
+						 process: (T) -> Void) {
+		switch option {
+		case .pre:  traversePreOrder(process: process)
+		case .in:   traverseInOrder(process: process)
+		case .post: traversePostOrder(process: process)
+		}
+	}
+	public func traverseInOrder(process: (T) -> Void) {
+		if case let .node(left, value, right) = self {
+			left.traverseInOrder(process: process)
+			process(value)
+			right.traverseInOrder(process: process)
+		}
+	}
+	
+	public func traversePreOrder(process: (T) -> Void) {
+		if case let .node(left, value, right) = self {
+			process(value)
+			left.traversePreOrder(process: process)
+			right.traversePreOrder(process: process)
+		}
+	}
+	
+	public func traversePostOrder(process: (T) -> Void) {
+		if case let .node(left, value, right) = self {
+			left.traversePostOrder(process: process)
+			right.traversePostOrder(process: process)
+			process(value)
+		}
+	}
+}
+
+extension BinaryTreeNode {
+	func invert() -> BinaryTreeNode {
+		if case let .node(left, value, right) = self {
+			return .node(right.invert(), value, left.invert())
+		} else {
+			return .empty
+		}
+	}
+}
+
+
+public class BinaryTree {
+	
+}
---
+++ Tests/SwiftDataStructureTests/TreeTests/BinaryTreeTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+import SwiftDataStructure
+
+class BinaryTreeTests: XCTestCase {
+	let node5 = BinaryTreeNode.node(.empty, "5", .empty)
+	let nodeA = BinaryTreeNode.node(.empty, "a", .empty)
+	let node10 = BinaryTreeNode.node(.empty, "10", .empty)
+	let node4 = BinaryTreeNode.node(.empty, "4", .empty)
+	let node3 = BinaryTreeNode.node(.empty, "3", .empty)
+	let nodeB = BinaryTreeNode.node(.empty, "b", .empty)
+
+	lazy var tree:BinaryTreeNode<String> = {
+		let aMinus10 = BinaryTreeNode.node(nodeA, "-", node10)
+		let timesLeft = BinaryTreeNode.node(node5, "*", aMinus10)
+		
+		// intermediate nodes on the right
+		let minus4 = BinaryTreeNode.node(.empty, "-", node4)
+		let divide3andB = BinaryTreeNode.node(node3, "/", nodeB)
+		let timesRight = BinaryTreeNode.node(minus4, "*", divide3andB)
+		return BinaryTreeNode.node(timesLeft, "+", timesRight)
+	}()
+	// intermediate nodes on the left
+
+	// root node
+	
+	func testBinaryTree() {
+		print(tree)
+		for option in [BinaryTreeNode<String>.TraverseOption.pre,.in,.post] {
+			print(option, terminator: ":\t")
+			tree.traverse(by: option) { (str) in
+				print(str, separator: "", terminator: " ")
+			}
+			print()
+		}
+	}
+	
+}
